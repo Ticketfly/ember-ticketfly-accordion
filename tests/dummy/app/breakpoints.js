095c43226bcdfd30d93ee6f816631ac6c199a9ea
BrianSipple <bsipple57@gmail.com>
2017-02-19 22:15:15 -0800

    Dummy app styling improvement for small widths

--- tests/dummy/app/breakpoints.js
+++ tests/dummy/app/breakpoints.js
@@ -2,6 +2,7 @@
  * Breakpoint threshold measurements in ems.
  */
 const THRESHOLDS = {
+  xSmall: 22,
   small: 30,
   medium: 49.125,
   large: 64,
@@ -18,18 +19,21 @@ const THRESHOLDS = {
  *      - for example, "{{#if media.isSmall}}"
  */
 export default {
-  small: `(max-width: ${THRESHOLDS.small}em)`,
+  xSmall: `(max-width: ${THRESHOLDS.xSmall}em)`,
+  small: `(min-width: ${THRESHOLDS.xSmall + 1}em) and (max-width: ${THRESHOLDS.small}em)`,
   medium: `(min-width: ${THRESHOLDS.small + 1}em) and (max-width: ${THRESHOLDS.medium}em)`,
   large: `(min-width: ${THRESHOLDS.medium + 1}em) and (max-width: ${THRESHOLDS.large}em)`,
   xLarge: `(min-width: ${THRESHOLDS.large + 1}em)`,  // AKA "monitor"
 
   // meta semantic helpers
+  greaterThanXSmall: `(min-width: ${THRESHOLDS.xSmall + 1}em)`,
   greaterThanSmall: `(min-width: ${THRESHOLDS.small + 1}em)`,
   greaterThanMedium: `(min-width: ${THRESHOLDS.medium + 1}em)`,
   greaterThanLarge: `(min-width: ${THRESHOLDS.large + 1}em)`,
   greaterThanXLarge: `(min-width: ${THRESHOLDS.xLarge + 1}em)`,
 
-  lessThanMedium: `(max-width: ${THRESHOLDS.small}em)`,
-  lessThanLarge: `(max-width: ${THRESHOLDS.medium}em)`,
-  lessThanXLarge: `(max-width: ${THRESHOLDS.large}em)`
+  lessThanSmall: `(max-width: ${THRESHOLDS.small}em)`,
+  lessThanMedium: `(max-width: ${THRESHOLDS.medium}em)`,
+  lessThanLarge: `(max-width: ${THRESHOLDS.large}em)`,
+  lessThanXLarge: `(max-width: ${THRESHOLDS.xLarge}em)`
 };
